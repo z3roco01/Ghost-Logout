88feb08095eb109f214c99ef66465a874676bddf
ihatecsv <drakeluce@gmail.com>
2023-08-22 17:44:47 -0300

    Backport to 1.19.2

--- src/main/java/z3roco01/ghostLogout/entity/GhostEntity.java
+++ src/main/java/z3roco01/ghostLogout/entity/GhostEntity.java
@@ -1,8 +1,8 @@
 package z3roco01.ghostLogout.entity;
 
 import com.mojang.authlib.GameProfile;
-import eu.pb4.polymer.core.api.entity.PolymerEntity;
-import eu.pb4.polymer.core.api.entity.PolymerEntityUtils;
+import eu.pb4.polymer.api.entity.PolymerEntity;
+import eu.pb4.polymer.api.entity.PolymerEntityUtils;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.data.DataTracker;
@@ -10,38 +10,34 @@ import net.minecraft.entity.data.TrackedData;
 import net.minecraft.entity.data.TrackedDataHandlerRegistry;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.nbt.NbtCompound;
+import net.minecraft.network.Packet;
 import net.minecraft.network.listener.ClientPlayPacketListener;
-import net.minecraft.network.packet.Packet;
 import net.minecraft.network.packet.s2c.play.PlayerListS2CPacket;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.text.Text;
 import net.minecraft.world.GameMode;
 import net.minecraft.world.World;
 
-import java.util.EnumSet;
 import java.util.List;
-import java.util.UUID;
 import java.util.function.Consumer;
 
-public class GhostEntity extends Entity implements PolymerEntity{
+public class GhostEntity extends Entity implements PolymerEntity {
     private static final TrackedData<Byte> PLAYER_MODEL_PARTS = DataTracker.registerData(PlayerEntity.class, TrackedDataHandlerRegistry.BYTE);
-    private static final EntityType type = EntityType.PLAYER;
-    private final ServerPlayerEntity player;
+    private static final EntityType<PlayerEntity> type = EntityType.PLAYER;
     public final String playerName;
-    private final UUID uuid;
 
     public GhostEntity(World world, ServerPlayerEntity player) {
         super(type, world);
-        this.player = player;
         this.playerName = player.getGameProfile().getName();
         uuid = player.getGameProfile().getId();
         this.setUuid(player.getGameProfile().getId());
     }
 
     @Override
-    public void onBeforeSpawnPacket(ServerPlayerEntity player, Consumer<Packet<?>> packetConsumer) {
-        PlayerListS2CPacket packet = PolymerEntityUtils.createMutablePlayerListPacket(EnumSet.of(PlayerListS2CPacket.Action.ADD_PLAYER, PlayerListS2CPacket.Action.UPDATE_LISTED));
+    public void onBeforeSpawnPacket(Consumer<Packet<?>> packetConsumer) {
+        PlayerListS2CPacket packet = new PlayerListS2CPacket(PlayerListS2CPacket.Action.ADD_PLAYER, List.of());
         GameProfile gameProfile = new GameProfile(this.getUuid(), playerName);
-        packet.getEntries().add(new PlayerListS2CPacket.Entry(this.getUuid(), gameProfile, false, 0, GameMode.ADVENTURE, null, null));
+        packet.getEntries().add(new PlayerListS2CPacket.Entry(gameProfile, 0, GameMode.ADVENTURE, Text.of(playerName), null));
         packetConsumer.accept(packet);
     }
 
@@ -51,8 +47,13 @@ public class GhostEntity extends Entity implements PolymerEntity{
     }
 
     @Override
-    public void modifyRawTrackedData(List<DataTracker.SerializedEntry<?>> data, ServerPlayerEntity player, boolean initial) {
-        data.add(DataTracker.SerializedEntry.of(PLAYER_MODEL_PARTS, (byte)0b00111111));
+    public void modifyTrackedData(List<DataTracker.Entry<?>> data) {
+        data.add(new DataTracker.Entry<>(PLAYER_MODEL_PARTS, (byte)0b00111111));
+    }
+
+    @Override
+    public EntityType<?> getPolymerEntityType() {
+        return type;
     }
 
     @Override
